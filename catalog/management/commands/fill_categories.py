5ae28cc03bc2669da1a58573903d51a5aa0bea93
Lev Polevshchikov <upmindset.28@gmail.com>
2023-09-09 01:15:26 +0300

    homework 3

---
+++ catalog/management/commands/fill_categories.py
@@ -0,0 +1,19 @@
+import json
+
+from django.core.management import BaseCommand
+
+from catalog.models import Category
+from config.settings import BASE_DIR
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        with open(BASE_DIR / 'data.json') as file:
+            category_list = json.load(file)
+
+        categories_for_create = []
+        for category in category_list:
+            categories_for_create.append(Category(category['pk'], category['fields']['name']))
+
+        Category.objects.bulk_create(categories_for_create)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    preview_img = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    price = models.IntegerField(verbose_name='цена')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    change_date = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
