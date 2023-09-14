8b193a50224ca4fa439bdd47b775c1ae62465be8
Lev Polevshchikov <upmindset.28@gmail.com>
2023-09-14 19:10:23 +0300

    homework 4

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -11,4 +18,12 @@ def contacts(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have new message from {name}({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product(request):
+    return render(request, 'catalog/product.html')
