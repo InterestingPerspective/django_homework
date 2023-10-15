159695157c89a4297c991286d32bc0e09118fc58
Lev Polevshchikov <upmindset.28@gmail.com>
2023-10-15 19:18:13 +0300

    homework 8 add cache_page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import contacts, ProductDetailView, ProductCreateView, VersionUpdateView, \
@@ -9,7 +10,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='view_product'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='update_product'),
     path('version/<int:pk>/update/', VersionUpdateView.as_view(), name='update_version'),
     path('version/', VersionCreateView.as_view(), name='create_version'),
